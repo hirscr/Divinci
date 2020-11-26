2f6d89b7dd9afe1dee7719abce0cd0e441a53cb8
hirscr <hirscr@me.com>
2020-11-26 15:35:42 -0400

    Small improvements mostly cleanup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Divinci',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='shandor',
+    author_email='',
+    description=''
+)
